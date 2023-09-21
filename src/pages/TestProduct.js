c59b2f9c9974c4a7c83e2fd2e139e034350452a8
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-09-21 16:25:33 +0300

    s

--- src/pages/TestProduct.js
+++ src/pages/TestProduct.js
@@ -26,7 +26,7 @@ const TestProduct = ({ isUser }) => {
   const [heightError, setheightError] = useState(false);
   // const
 const [SubOption , setSubOption] = useState(null)
-
+const [FinalOption , setFinalOption] = useState(null)
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   useEffect(() => {
@@ -167,7 +167,6 @@ const [SubOption , setSubOption] = useState(null)
                                   key={`${ele} ${idx}`}
                                 >
                                   <AiOutlineCheck />
-                                  {ele}
                                 </div>
                               );
                             })}
@@ -433,6 +432,7 @@ const [SubOption , setSubOption] = useState(null)
                                                   {/* -------------------------------------- */}
                                               <div style={{textAlign : 'center'}}
                                                 onClick={() => {
+                                                  setFinalOption(item.childrens)
                                                   const Option_data = {
                                                     section_id: parseFloat(
                                                       item.section_id
@@ -452,8 +452,109 @@ const [SubOption , setSubOption] = useState(null)
                                                         parseFloat(item.section_id)
                                                     );
                                                     const New_selected = [
-                                                      ...old_selected,
                                                       Option_data,
+                                                    ];
+                                                    setSelected(New_selected);
+
+                                                    if (All_ids.length <= 0) {
+                                                      dispatch(
+                                                        getProductSummery(
+                                                          `?product_id=${id2}&options[0]=${parseFloat(
+                                                            item.id
+                                                          )}&width=${width}&height=${height}&quantity=${quantity}`
+                                                        )
+                                                      );
+                                                    } else {
+                                                      // SendData();
+                                                      SendDataOption(
+                                                        New_selected
+
+                                                      );
+                                                    }
+                                                  }
+                                                }}
+                                              >
+                                                {item.image ? (
+                                                  <>
+                                                    <div
+                                                      className="Card_Image"
+                                                      style={{
+                                                        borderColor: All_ids.includes(
+                                                          item.id
+                                                        )
+                                                          ? "#0a3565"
+                                                          : "#d1d1d1",
+                                                      }}
+                                                      
+                                                    >
+                                                        <img
+                                                          src={item.image}
+                                                          alt=""
+                                                          width={100} height={100}
+                                                        />
+                                                      </div>
+                                                      
+                                                      <h3>{item.name}</h3>
+                                                  
+                                                  </>
+                                                ) : (
+                                                  <>
+                                                    <div
+                                                      className="Chose text-center "
+                                                      style={{
+                                                        borderColor: All_ids.includes(
+                                                          item.id
+                                                        )
+                                                          ? "#0a3565"
+                                                          : "#d1d1d1",
+                                                      }}
+                                                    >
+                                                      {item.name}
+                                                    </div>
+                                                    <p>{item.description}</p>
+                                                  </>
+                                                )}
+                                              </div>
+                                                  {/* -------------------------------------- */}
+                                                  
+                                                </div>
+                                              )
+                                            })}
+                                                      </div>
+                                                   : null}
+                                     </div>
+               {/* ********************************************************************************************   */}
+               <div className="row">
+                                                   {FinalOption ? 
+                                                    <div className="row">
+                                                      {FinalOption.map((item)=>{
+                                              return (
+                                                <div key={item.id} className="col-6">
+                                                  {/* -------------------------------------- */}
+                                              <div style={{textAlign : 'center'}}
+                                                onClick={() => {
+                                                  const Option_data = {
+                                                    section_id: parseFloat(
+                                                      item.section_id
+                                                    ),
+                                                    Option_id: parseFloat(item.id),
+                                                  };
+                                                  if (
+                                                    selected_op.indexOf(
+                                                      Option_data
+                                                    ) === -1
+                                                  ) {
+                                                    const old_selected = selected_op.filter(
+                                                      (op) =>
+                                                        parseFloat(
+                                                          op.section_id
+                                                        ) !==
+                                                        parseFloat(item.section_id)
+                                                    );
+                                                    const New_selected = [
+                                                      
+                                                      Option_data,
+                                                      
                                                     ];
                                                     setSelected(New_selected);
 
@@ -516,12 +617,14 @@ const [SubOption , setSubOption] = useState(null)
                                                 )}
                                               </div>
                                                   {/* -------------------------------------- */}
+                                                  
                                                 </div>
                                               )
                                             })}
                                                       </div>
                                                    : null}
-                                                      </div>
+                                     </div>
+               {/* ********************************************************************************************   */}
 
                                     </div>
                                   </div>
@@ -606,7 +709,7 @@ const [SubOption , setSubOption] = useState(null)
                         return (
                           <div className="d-flex  " key={ele.name}>
                             <h5>{ele.section}</h5>
-                            <span> {ele.name}</span>
+                            <span> {SubOption.name}</span>
                           </div>
                         );
                       })}
